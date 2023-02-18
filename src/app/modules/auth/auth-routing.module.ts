2ecd14e0f2a12bc5f00e63193b1a19ef81516de6
Noelí Rodríguez <noe.rmorales98@gmail.com>
2023-02-18 11:37:09 -0500

    add register

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterFormComponent } from 'src/app/modules/auth/components/register-form/register-form.component';
 import { LoginPageComponent } from 'src/app/modules/auth/page/login-page/login-page.component';
 
 
@@ -10,7 +11,7 @@ const routes: Routes = [
   },
   {
     path: 'register',
-    component:  LoginPageComponent
+    component:  RegisterFormComponent
   }
 ];
 
--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -4,11 +4,14 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RegisterFormComponent } from './components/register-form/register-form.component';
 
 
 
 @NgModule({
   declarations: [
+  
+    RegisterFormComponent
   ],
   imports: [
     CommonModule,
